13f95581e5713a7a8fab818027e5af2be5e6c17c
Chris Zucchet <Chris Zucchet@au.aap.ad.pwcinternal.com>
2018-08-29 14:55:41 +1000

    Model build

--- _2_create_model.R
+++ _2_create_model.R
@@ -1,5 +1,5 @@
 setwd("C:/Users/Chris Zucchet/Documents/AFL-Prediction-task")
-library(tidyverse);library(DBI);library(xgboost);library(recipes);library(rsample);library(RSQLite);library(purrr);library(e1071);library(DMwR)
+library(DMwR);library(tidyverse);library(DBI);library(xgboost);library(recipes);library(rsample);library(RSQLite);library(purrr);library(e1071);library(digest);library(mlr);library(parallelMap)
 range01 <- function(x){(x-min(x))/(max(x)-min(x))}
 con = dbConnect(SQLite(), "PlayerRecords.sqlite")
 num_cols = c("KI","MK","HB","GL","BH","HO","TK","RB","IF","CL","CG","FF","FA","BR","CP","UP","CM","MI","one_pc","BO","GA","game_played","Year","Round","Diff","Age_Years","Games","PercentWon")
@@ -17,8 +17,8 @@ records_t3 = records_t2 %>%
          games_1_10 =  ifelse(Games <= 10, 1,0),games_11_50 = ifelse(Games > 10 & Games <= 50, 1,0),games_51_more = ifelse(Games > 50, 1,0),
          dt_score = (KI*3)+(HB*2)+(MK*3)+(GL*6)+(BH*1)+(TK*3)+(HO*1)+(FF*1)+(FA*-3)+(GA*3),
          error_rate = CG/(KI+HB),
-         fairness = FF-FA);rm(records_t2);rm(home_team);rm(away_team)
-records_t4 = records_t3 %>% left_join(home_team) %>% left_join(away_team);rm(records_t3)
+         fairness = FF-FA)
+records_t4 = records_t3 %>% left_join(home_team) %>% left_join(away_team);rm(records_t3);rm(home_team);rm(away_team);rm(records_t2)
 records_t5 = records_t4 %>% mutate(interstate = ifelse(home_state != away_state, 1,0));rm(records_t4) 
 
 rec_split = records_t5 %>% split(.$ID)
@@ -28,21 +28,42 @@ for(i in 1:length(rec_split)){
     }
   }
 rec_all = bind_rows(rec_split) %>% select(-number,-Player,-DI,-ID);rm(rec_split)
-br_0_1 = rec_all %>% filter(BR == 0 | BR == 1) %>% droplevels() %>% mutate(BR = as.factor(BR));
-br_0_2 = rec_all %>% filter(BR == 0 | BR == 2) %>% droplevels() %>% mutate(BR = as.factor(BR));
-br_0_3 = rec_all %>% filter(BR == 0 | BR == 3) %>% droplevels() %>% mutate(BR = as.factor(BR));
 
-smote_0_1 = SMOTE(BR~., br_0_1, perc.over = 1/(table(br_0_1$BR)[2]/sum(table(br_0_1$BR))),
-              perc.under = 1/(table(br_0_1$BR)[1]/sum(table(br_0_1$BR))))
+rec_train =  recipe(BR ~., rec_all) %>%
+  step_dummy(Team,Home_Away,Opponent, home_state, away_state) %>%
+  step_other(Venue, threshold = .2) %>%
+  step_dummy(Venue) %>% 
+  prep() %>%
+  bake(newdata =  rec_all)
+
+br_0_1 = rec_train %>% filter(BR == 0 | BR == 1) %>% droplevels()%>% mutate(BR = as.factor(BR)) %>% data.frame();smote_0_1 = SMOTE(BR~., br_0_1, perc.over = sum(table(br_0_1$BR))/table(br_0_1$BR)[2]*100, perc.under = sum(table(br_0_1$BR))/table(br_0_1$BR)[1]*100)
+br_0_2 = rec_train %>% filter(BR == 0 | BR == 2) %>% droplevels()%>% mutate(BR = as.factor(BR)) %>% data.frame();smote_0_2 = SMOTE(BR~., br_0_2, perc.over = sum(table(br_0_2$BR))/table(br_0_2$BR)[2]*100, perc.under = sum(table(br_0_2$BR))/table(br_0_2$BR)[1]*100)
+br_0_3 = rec_train %>% filter(BR == 0 | BR == 3) %>% droplevels()%>% mutate(BR = as.factor(BR)) %>% data.frame();smote_0_3 = SMOTE(BR~., br_0_3, perc.over = sum(table(br_0_3$BR))/table(br_0_3$BR)[2]*100, perc.under = sum(table(br_0_3$BR))/table(br_0_3$BR)[1]*100)
+bal_data = rbind(smote_0_1[smote_0_1$BR == 1,],smote_0_2[smote_0_2$BR == 2,],smote_0_3[smote_0_3$BR == 3,],rec_train[rec_train$BR == 0,])
+bal_data$BR = as.numeric(bal_data$BR)-1 
+bal_data_all = bal_data %>% na.omit()
 
 
-rec_wc = recipe(winner~., data = rec_all) %>%
-  #            step_num2factor(matchID) %>%
-  #            step_discretize(team_1_rank, options = list(cuts = 8)) %>%
-  #            step_discretize(team_2_rank, options = list(cuts = 8)) %>%
-  step_log(team_1_rank) %>%   step_scale(team_1_rank) %>%
-  
+train_matrix <- xgb.DMatrix(data = as.matrix(bal_data_all %>%select(-BR)),
+                            label = as.matrix(bal_data_all %>%select(BR)))
+param <- list(objective = "multi:softprob",eval_metric = "merror",num_class = 4,max_depth = 5,eta = 0.025,
+              gamma = 0.01, min_child_weight = 4)
+cv.nround = 600;cv.nfold = 5
+model_cv = xgb.cv(data=train_matrix, params = param,  
+               nfold=cv.nfold, nrounds=cv.nround,
+               verbose = 3)
+min_error = min(model_cv$evaluation_log$test_merror_mean)
+min_error_index = which.min(model_cv$evaluation_log$test_merror_mean)
 
-         
+xgb_model <- xgb.train (params = param,
+                         data = train_matrix,
+                         nrounds = min_error_index,
+                         missing=NaN,
+                         verbose = 3,
+                         print_every_n = 5,
+                         maximize = F)
+xgb.save(xgb_model,'afl_model')
+
+
 
 
